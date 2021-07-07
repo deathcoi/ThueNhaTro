a2deb39523663faea409ba9699bf1f302ca43f38
deathcoi <deathcoi1711@gmail.com>
2021-07-07 19:14:01 +0700

    create again

---
+++ ThueNha/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThueNha.MvcApplication" Language="C#" %>
---
+++ ThueNha/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThueNha.Startup))]
+namespace ThueNha
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
